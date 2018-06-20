b27902c6afae8c33e473cd1fe3e1fdfd97b78769
shaneleblanc <36498002+shaneleblanc@users.noreply.github.com>
2018-06-20 15:49:43 -0700

    Create modulus.py

---
+++ modulus.py
@@ -0,0 +1,8 @@
+# It frustrates you more than you'd like to admit that the modulus operator in Python can be applied to non-integer values. When you write code, you expect the result of the modulus operator to always be an integer, but thanks to Python this isn't always the case.
+
+# To fix this, you've decided to write your own modulus operator as a function. Your task is to implement a function that, given a number n, returns -1 if this number is not an integer and n % 2 otherwise. It is guaranteed that if the number represents an integer, it will be written without a decimal point.
+def modulus(n):
+    if float(n).is_integer():
+        return n % 2
+    else:
+        return -1
